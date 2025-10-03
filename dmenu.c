a2fec8ca8b5273ca4d50ed52edcbdf33c9382cfc
Erick Lievana Poy <ericklievana@gmail.com>
2025-10-02 22:09:58 -0600

    Remove indenting of options to appear at the same space as prompt

--- dmenu.c
+++ dmenu.c
@@ -191,9 +191,9 @@ drawmenu(void)
 		for (item = curr; item != next; item = item->right, i++)
 			drawitem(
 				item,
-				x + ((i / lines) *  ((mw - x) / columns)),
+				x + ((i / lines) *  ((mw) / columns)) - promptw,
 				y + (((i % lines) + 1) * bh),
-				(mw - x) / columns
+				(mw) / columns
 			);
 	} else if (matches) {
 		/* draw horizontal list */
